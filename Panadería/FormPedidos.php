15f1c7812d558875406b1fd32de09196718b80e0
SilkierKnave055 <alejandro.marquez.garcia.al@iespoligonosur.org>
2024-03-12 12:21:58 +0100

    modified:   "Panader\303\255a/FormPedidos.php"

--- Panadería/FormPedidos.php
+++ Panadería/FormPedidos.php
@@ -16,20 +16,25 @@
         </div>
         <div class="form-group">
             <?php
+            //Creamos las variables para la conexion a la BBDD
             $servername = "localhost";
             $username = "phpmyadmin";
             $password = "phpmyadmin";
+            //Hacemos la conexion a la BBDD
             $conexion = new mysqli($servername, $username, $password);
+            //Si no conseguimos la conexion, terminamos la conexion, sino, lanzamos mensaje de conexion exitosa.
             if (!$conexion) {
                 die("Conexión fallida:" . $conexion->connect_error);
             }
 
+            //Seleccionamos la base de datos con los datos para la conexion y el nombre de la BBDD
             mysqli_select_db($conexion, "panaderia");
 
+            //Hacemos una consulta para la base de datos
             $consultas = "SELECT nombre FROM Pedidos,Clientes WHERE Clientes.nombre = Pedidos.nombre_cliente";
-
             $registros = mysqli_query($conexion, $consultas);
 
+            //Al ser un lenguaje de html lo metemos dentro de un echo para que lo coja correctamente
             echo "<label for='seleccionar'>Selecciona tu nombre: </label>";
             echo "<select class='form-control' name='seleccionar' id='seleccionar'>";
             while ($registro = mysqli_fetch_row($registros)) {
